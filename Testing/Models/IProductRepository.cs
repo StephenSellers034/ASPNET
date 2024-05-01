464fef9cb9bd08a50dbd4cbe822e5d37b4a504b4
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-05-01 15:48:13 -0400

    part 2

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+using System.Data;
+using Dapper;
+
+namespace Testing.Models
+{
+	public interface IProductRepository 
+	{
+		public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        
+
+
+
+
+
+    }
+}
+
